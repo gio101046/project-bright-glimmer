d2f9c8496347b9ed2a8d53321cc51fae4c3d38ad
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2019-04-19 22:00:29 +0000

    Setup initial solution structure fo application

---
+++ BrightGlimmer.Api/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrightGlimmer.Cqrs.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrightGlimmer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public CourseController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "";
+        }
+    }
+}
--- BrightGlimmer.Api/Startup.cs
+++ BrightGlimmer.Api/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BrightGlimmer.Data;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,8 @@ namespace BrightGlimmer.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMediatR();
+            services.AddMediatR(typeof(Cqrs.Cqrs).Assembly); // Registers handlers in Cqrs project
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
